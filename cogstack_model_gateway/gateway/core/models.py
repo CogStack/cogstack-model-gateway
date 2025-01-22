49712c60c30c8a1ac846ab19fd7200010637f92b
Phoevos Kalemkeris <phoevos.kalemkeris@ucl.ac.uk>
2025-01-22 13:34:59 +0000

    fix: List running models in specified project only
    Signed-off-by: Phoevos Kalemkeris <phoevos.kalemkeris@ucl.ac.uk>

--- cogstack_model_gateway/gateway/core/models.py
+++ cogstack_model_gateway/gateway/core/models.py
@@ -1,3 +1,4 @@
+import os
 from functools import lru_cache
 
 import docker
@@ -6,13 +7,25 @@ from mlflow.entities.model_registry import RegisteredModel
 
 IS_MODEL_LABEL = "org.cogstack.model-serve"
 MODEL_URI_LABEL = "org.cogstack.model-serve.uri"
+PROJECT_NAME_LABEL = "com.docker.compose.project"
 SERVICE_NAME_LABEL = "com.docker.compose.service"
+
+CMS_PROJECT_ENV_VAR = "CMS_PROJECT_NAME"
 
 
 @lru_cache
 def get_running_models() -> list[dict]:
     client = docker.from_env()
-    containers = client.containers.list(filters={"label": IS_MODEL_LABEL})
+    cms_project = os.getenv(CMS_PROJECT_ENV_VAR)
+    if not cms_project:
+        raise ValueError(f"Environment variable {CMS_PROJECT_ENV_VAR} is not set.")
+
+    containers = client.containers.list(
+        filters={
+            "status": "running",
+            "label": [IS_MODEL_LABEL, f"{PROJECT_NAME_LABEL}={cms_project}"],
+        }
+    )
     return [
         {"name": c.labels.get(SERVICE_NAME_LABEL, c.name), "uri": c.labels.get(MODEL_URI_LABEL)}
         for c in containers
