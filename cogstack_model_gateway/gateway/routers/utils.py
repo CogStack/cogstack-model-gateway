c498dfbf5e589a3f3c4a33adc1570df1fd0f7734
Phoevos Kalemkeris <phoevos.kalemkeris@ucl.ac.uk>
2025-02-03 14:14:55 +0000

    gw: Add helper for validating model names
    Validate model names, ensuring that they adhere to the Docker container
    name restrictions:
    * Must start with an alphanumeric character
    * Can contain alphanumeric characters, underscores, dots, and dashes
    
    Signed-off-by: Phoevos Kalemkeris <phoevos.kalemkeris@ucl.ac.uk>

--- cogstack_model_gateway/gateway/routers/utils.py
+++ cogstack_model_gateway/gateway/routers/utils.py
@@ -1,8 +1,15 @@
+import re
 from typing import Annotated
 
-from fastapi import Header, Request
+from fastapi import Header, HTTPException, Path, Request
 
 from cogstack_model_gateway.common.utils import parse_content_type_header
+
+MODEL_NAME_REGEX = r"^[a-zA-Z0-9][a-zA-Z0-9_.-]+$"
+VALID_MODEL_DESCRIPTION = (
+    "The model name must start with an alphanumeric character and can only contain"
+    " alphanumeric characters, underscores (_), dots (.), and dashes (-)."
+)
 
 
 def get_query_params(request: Request) -> dict[str, str]:
@@ -12,3 +19,23 @@ def get_query_params(request: Request) -> dict[str, str]:
 def get_content_type(content_type: Annotated[str, Header()]) -> str:
     parsed_content_type, _ = parse_content_type_header(content_type)
     return parsed_content_type
+
+
+def validate_model_name(
+    model_name: Annotated[
+        str,
+        Path(
+            ...,
+            description=(
+                "The name of the model to deploy (used as the container name)."
+                f" {VALID_MODEL_DESCRIPTION}"
+            ),
+        ),
+    ],
+) -> str:
+    if not re.match(MODEL_NAME_REGEX, model_name):
+        raise HTTPException(
+            status_code=400,
+            detail=f"Invalid model name. {VALID_MODEL_DESCRIPTION}",
+        )
+    return model_name
